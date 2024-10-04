b06992dd1e0578e3101ef5d8959612da091f00fd
Jeziel-BG <Jezielgon@hotmail.com>
2024-10-04 09:05:51 -0700

    Preping for back ground animations

--- portfolio-site/src/Components/Skills/Skills.jsx
+++ portfolio-site/src/Components/Skills/Skills.jsx
@@ -3,28 +3,37 @@ import "./Skills.css";
 export function Skills(){
     return(
         <div id="Skills-Main">
-            <div id="Header-Content">
-                <h1 id="Section-Header">Skills & Technology</h1>
-                <span id="Section-Subtext">All challenges are solvable with the right tools. Here are the tools I commonly use.</span>
-            </div>
-            
-            <div id="List-Container">
 
-                <ul id="List1">
-                    <li ><img src="images/HTML-logo.png" id="HTML5" ></img></li>
-                    <li ><img src="images/CSS-logo.png" id="CSS" ></img></li>
-                    <li ><img src="images/javascript-logo.png" id="javascript" ></img></li>
-                    <li ><img src="images/react-logo.png" id="react" ></img></li>
-                </ul>
+            <div id="Content-Container">
+                <div id="Header-Content">
+                    <h1 id="Section-Header">Skills & Technology</h1>
+                    <span id="Section-Subtext">All challenges are solvable with the right tools. Here are the tools I commonly use.</span>
+                </div>
+                
+                <div id="List-Container">
 
-                <ul id="List2">
-                    <li ><img src="images/python-logo.png"  id="python"></img></li>
-                    <li ><img src="images/java-logo.png" id="java" ></img></li>
-                    <li ><img src="images/C-logo.png" id="c"></img></li>
-                </ul>
+                    <ul id="List1">
+                        <li ><img src="images/HTML-logo.png" id="HTML5" ></img></li>
+                        <li ><img src="images/CSS-logo.png" id="CSS" ></img></li>
+                        <li ><img src="images/javascript-logo.png" id="javascript" ></img></li>
+                        <li ><img src="images/react-logo.png" id="react" ></img></li>
+                    </ul>
+
+                    <ul id="List2">
+                        <li ><img src="images/python-logo.png"  id="python"></img></li>
+                        <li ><img src="images/java-logo.png" id="java" ></img></li>
+                        <li ><img src="images/C-logo.png" id="c"></img></li>
+                    </ul>
 
                 </div>
+            </div>
 
+            <svg id="Blob-Container">
+                <rect width="10vw" height="10vw" x="10" y="10" rx="20" ry="20" fill="blue" className="Blob Blue"/>
+                <rect width="10vw" height="10vw" x="10" y="10" rx="20" ry="20" fill="red" className="Blob Red"/>
+                <rect width="10vw" height="10vw" x="10" y="10" rx="20" ry="20" fill="yellow" className="Blob Yellow"/>
+                <rect width="10vw" height="10vw" x="10" y="10" rx="20" ry="20" fill="green" className="Blob Green"/>
+            </svg>
             
         </div>
     )
