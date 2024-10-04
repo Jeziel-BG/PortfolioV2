92c2fd28f4c614e20937a1e584dcfb7d31918512
Jeziel-BG <Jezielgon@hotmail.com>
2024-10-03 22:47:29 -0700

    Reworked Skills section

--- portfolio-site/src/Components/Skills/Skills.jsx
+++ portfolio-site/src/Components/Skills/Skills.jsx
@@ -10,16 +10,17 @@ export function Skills(){
             
             <div id="List-Container">
 
-                <ul id="List">
-                    <li ><img src="images/python-logo.png"  id="python"></img></li>
-                    <li ><img src="images/java-logo.png" id="java" ></img></li>
+                <ul id="List1">
                     <li ><img src="images/HTML-logo.png" id="HTML5" ></img></li>
                     <li ><img src="images/CSS-logo.png" id="CSS" ></img></li>
                     <li ><img src="images/javascript-logo.png" id="javascript" ></img></li>
                     <li ><img src="images/react-logo.png" id="react" ></img></li>
-                    <li ><img src="images/C-logo.png" id="c"></img></li>
+                </ul>
 
-                    
+                <ul id="List2">
+                    <li ><img src="images/python-logo.png"  id="python"></img></li>
+                    <li ><img src="images/java-logo.png" id="java" ></img></li>
+                    <li ><img src="images/C-logo.png" id="c"></img></li>
                 </ul>
 
                 </div>
