418b87ff3a74b800e790b5046970396288aa0a15
JezielBG-UofA <126594724+JezielBG-UofA@users.noreply.github.com>
2024-10-11 16:15:34 -0700

    Adding a Nav bar

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -5,19 +5,78 @@ import { Projects } from './components/Projects/Projects';
 import { Skills } from "./components/Skills/Skills";
 import { Education } from './components/Education/Education';
 import { Contact } from './components/Contact/Contact';
+import React, { useState } from "react";
+import { useRef } from "react";
 
 
 function App() {
+  const introduction = useRef(null);
+  const projects = useRef(null);
+  const skills = useRef(null);
+  const contact = useRef(null);
+
+  const scrollToSection = (elementRef) => {
+    window.scrollTo({
+      top: elementRef.current.offsetTop,
+      behavior: "smooth",
+    });
+  };
+
+  const hideNav = () => {
+    const nav = document.getElementById("nav");
+
+    let lastScrollY = window.scrollY;
+    window.addEventListener("scroll", () => {
+      if (lastScrollY < window.scrollY) {
+        nav.classList.add("hidden");
+      } else {
+        nav.classList.remove("hidden");
+      }
+
+      lastScrollY = window.scrollY;
+    });
+  };
+
+  window.addEventListener("scroll", hideNav);
+
   return (
     <div id="Main">
-      
+    
+      <div className="Site-Container" onScroll={hideNav}>
+        <ul id="nav" className="navbar">
+          <li className="introduction">
+            <button className='NavButton' onClick={() => scrollToSection(introduction)}> Introduction </button>
+          </li>
+    
+          <li className="projects">
+            <button className='NavButton' onClick={() => scrollToSection(projects)}> Projects </button>
+          </li>
 
-      <Introduction />
-      <Projects />
-      <Skills />
-      {//<<Education />
-      }
-      <Contact />
+          <li className="skills">
+            <button className='NavButton' onClick={() => scrollToSection(skills)}> Skills </button>
+          </li>
+          
+          <li className="contact">
+            <button className='NavButton' onClick={() => scrollToSection(contact)}> Contact </button>
+          </li>
+        </ul>
+
+        <div ref={introduction} className="introduction-section">
+          <Introduction />
+        </div>
+
+        <div ref={projects} className="project-section">
+          <Projects />
+        </div>
+
+        <div ref={skills} className="skills-section">
+          <Skills />
+        </div>
+
+        <div ref={contact} className="contact-section">
+          <Contact />
+        </div>
+      </div>
 
       
     </div>
--- portfolio-site/src/Components/Contact/Contact.jsx
+++ portfolio-site/src/Components/Contact/Contact.jsx
@@ -14,22 +14,22 @@ export function Contact(){
                 <form id='Contact-Form' method='post'>
                     <div id="name">
                         <label htmlFor='senderName'>Full Name:</label>
-                        <input type='text' id='senderName' className='senderName' placeholder='John Doe'></input>
+                        <input type='text' id='senderName' className='senderName' placeholder='John Doe' required></input>
                     </div>
 
                     <div>
                         <label htmlFor='senderEmail'>Email Address:</label>
-                        <input type='email' id='senderEmail' className='senderEmail' placeholder='example@gmail.com'></input>
+                        <input type='email' id='senderEmail' className='senderEmail' placeholder='example@gmail.com' required></input>
                     </div>
                     
                     <div>
                         <label htmlFor='subjectLine'>Subject Line:</label>
-                        <input type='text' id='subjectLine' className='subjectLine' placeholder='Your subject'></input>
+                        <input type='text' id='subjectLine' className='subjectLine' placeholder='Your subject' required></input>
                     </div>
 
                     <div>
                         <label htmlFor='message'>Email Message:</label>
-                        <textarea id='message' className='message' placeholder='Your email message.'></textarea>
+                        <textarea id='message' className='message' placeholder='Your email message.' required></textarea>
                     </div>
                     <input type="submit" value="Send" id="SendButton"></input>
                 </form>
