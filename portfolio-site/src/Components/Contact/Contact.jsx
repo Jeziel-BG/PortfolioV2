8abc168a7faacf546c62e83e9c10125203a093b9
Jeziel-BG <Jezielgon@hotmail.com>
2024-10-13 21:57:01 -0700

    Activated Contact Form

--- portfolio-site/src/Components/Contact/Contact.jsx
+++ portfolio-site/src/Components/Contact/Contact.jsx
@@ -1,6 +1,25 @@
 import styles from './Contact.css';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 export function Contact(){
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm(process.env.REACT_APP_emailSenderServiceID , process.env.REACT_APP_emailSenderTempleteKey, form.current, process.env.REACT_APP_emailSenderPublicKey)
+        .then((result) => {
+            console.log(result.text);
+            alert("Email sent successfully.")
+        }, (error) => {
+            console.log(error.text);
+            alert("Email was not sent successfully.")
+        });
+    };
+
+
     return(
         <div id='Contact-Main'>
             <div id="Content-Container">
@@ -11,25 +30,25 @@ export function Contact(){
 
 
 
-                <form id='Contact-Form' method='post'>
+                <form id='Contact-Form' method='post' ref={form} onSubmit={sendEmail}>
                     <div id="name">
                         <label htmlFor='senderName'>Full Name:</label>
-                        <input type='text' id='senderName' className='senderName' placeholder='John Doe' required></input>
+                        <input type='text' id='senderName' className='senderName' placeholder='John Doe' name='user_name'required></input>
                     </div>
 
                     <div>
                         <label htmlFor='senderEmail'>Email Address:</label>
-                        <input type='email' id='senderEmail' className='senderEmail' placeholder='example@gmail.com' required></input>
+                        <input type='email' id='senderEmail' className='senderEmail' placeholder='example@gmail.com' name='user_email' required></input>
                     </div>
                     
                     <div>
                         <label htmlFor='subjectLine'>Subject Line:</label>
-                        <input type='text' id='subjectLine' className='subjectLine' placeholder='Your subject' required></input>
+                        <input type='text' id='subjectLine' className='subjectLine' placeholder='Your subject' name='subject_line' required></input>
                     </div>
 
                     <div>
                         <label htmlFor='message'>Email Message:</label>
-                        <textarea id='message' className='message' placeholder='Your email message.' required></textarea>
+                        <textarea id='message' className='message' placeholder='Your email message.' name='message' required></textarea>
                     </div>
                     <input type="submit" value="Send" id="SendButton"></input>
                 </form>
